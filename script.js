5717605b2488288d8b542459b93b1207a7e4409e
Akos Somodi <akossomodi@yahoo.com>
2020-03-16 14:35:58 +0100

    finish the js file

--- script.js
+++ script.js
@@ -18,18 +18,32 @@ function updatePlayIcon() {
   if (video.paused) {
     play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
   } else {
-    play.innerHTML = '<i class="fa fa-stop fa-2x"></i>';
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
   }
 }
 
 // Update progress and timestamp
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
